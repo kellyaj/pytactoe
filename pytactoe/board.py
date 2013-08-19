95b55eab7dbb804e42b0dd69e5723889c44b6372
Andrew Kelly <kellyaj@gmail.com>
2013-08-19 17:20:32 -0500

    refactors board spots creation and includes get_rows method

--- pytactoe/board.py
+++ pytactoe/board.py
@@ -1,17 +1,24 @@
 class Board(object):
 
-    def __init__(self, size):
-        self.spots = ["" for n in range(size)]
+    def __init__(self):
+        self.spots = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
     def place_move(self, mark, spot):
         self.spots[spot - 1] = mark
 
     def spot_is_taken(self, spot):
-        return self.spots[spot - 1] != ""
+        return isinstance(self.spots[spot - 1], str)
 
     def available_spots(self):
         available_spots = []
         for idx, val in enumerate(self.spots):
-            if val == "":
+            if isinstance(val, int):
                 available_spots.append(idx + 1)
         return available_spots
+
+    def get_all_rows(self):
+        rows = []
+        rows.append(self.spots[0:3])
+        rows.append(self.spots[3:6])
+        rows.append(self.spots[6:9])
+        return rows
--- pytactoe/test/test_board.py
+++ pytactoe/test/test_board.py
@@ -4,10 +4,10 @@ from pytactoe.board import Board
 class BoardTests(unittest.TestCase):
 
     def setUp(self):
-        self.board = Board(9)
+        self.board = Board()
 
     def test_board_initializes_with_spots(self):
-        self.assertEqual(["" for n in range(9)], self.board.spots)
+        self.assertEqual([1, 2, 3, 4, 5, 6, 7, 8, 9], self.board.spots)
 
     def test_placing_moves_on_board(self):
         self.board.place_move("X",1)
@@ -22,3 +22,8 @@ class BoardTests(unittest.TestCase):
         self.assertEqual([1, 2, 3, 4, 5, 6, 7, 8, 9], self.board.available_spots())
         self.board.place_move("X", 5)
         self.assertEqual([1, 2, 3, 4, 6, 7, 8, 9], self.board.available_spots())
+
+    def test_getting_list_of_rows(self):
+        all_rows = self.board.get_all_rows()
+        expected_rows = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        self.assertEqual(expected_rows, all_rows)
