245382e74c6f5956edd6f6bf35bcb5f29a7315ff
Andrew Kelly <kellyaj@gmail.com>
2013-08-19 20:34:14 -0500

    allows scorer to check for a win using rows, columns, diagonals

--- pytactoe/scorer.py
+++ pytactoe/scorer.py
@@ -3,3 +3,11 @@ class Scorer(object):
     @staticmethod
     def row_won(row):
         return len(row) != len(set(row))
+
+    @staticmethod
+    def is_game_won(rows, columns, diagonals):
+        spaces = [rows, columns, diagonals]
+        if True in map(lambda x: Scorer.row_won(x), spaces):
+          return True
+        else:
+          return False
--- pytactoe/test/test_scorer.py
+++ pytactoe/test/test_scorer.py
@@ -6,6 +6,15 @@ class ScorerTests(unittest.TestCase):
     def test_evaluating_rows_as_wins(self):
         unwinning_row = ["X", 2, "O"]
         winning_row = ["X", "X", "X"]
+
         self.assertFalse(Scorer.row_won(unwinning_row))
         self.assertTrue(Scorer.row_won(winning_row))
 
+    def test_evaluating_a_board_as_won(self):
+        not_win_row = [1, 2, 3]
+        win_row = ["X", "X", "X"]
+
+        self.assertFalse(Scorer.is_game_won(not_win_row, not_win_row, not_win_row))
+        self.assertTrue(Scorer.is_game_won(win_row, not_win_row, not_win_row))
+        self.assertTrue(Scorer.is_game_won(not_win_row, win_row, not_win_row))
+        self.assertTrue(Scorer.is_game_won(not_win_row, not_win_row, win_row))
