8760f2817b5f61be6750828f40f47523aca1ce12
Andrew Kelly <kellyaj@gmail.com>
2013-08-20 23:07:46 -0500

    Computers can choose moves from lists of available spots

--- pytactoe/computer.py
+++ pytactoe/computer.py
@@ -4,3 +4,6 @@ class Computer(Player):
 
     def __init__(self, mark):
         super(Computer, self).__init__(mark)
+
+    def get_move(self, available_spots):
+        return available_spots[0]
--- pytactoe/test/test_computer.py
+++ pytactoe/test/test_computer.py
@@ -3,7 +3,19 @@ from pytactoe.computer import Computer
 
 class ComputerTests(unittest.TestCase):
 
-    def test_computer_initializes_with_a_mark(self):
-        computer = Computer("O")
+    def setUp(self):
+        self.computer = Computer("O")
+        self.available_spots = [2, 3, 4, 5, 6, 7, 8, 9]
 
-        self.assertEqual("O", computer.mark)
+    def test_computer_initializes_with_a_mark(self):
+        self.assertEqual("O", self.computer.mark)
+
+    def test_computer_chooses_integers(self):
+        chosen_move = self.computer.get_move(self.available_spots)
+
+        self.assertTrue(isinstance(chosen_move, int))
+
+    def test_computer_chooses_moves_from_available_spots(self):
+        chosen_move = self.computer.get_move(self.available_spots)
+
+        self.assertTrue(chosen_move in self.available_spots)
