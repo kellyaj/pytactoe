5c40baf303cb1d85d2c98daa9d4d8d9f69bb7878
Andrew Kelly <kellyaj@gmail.com>
2013-08-20 20:59:04 -0500

    gives Human an io and uses that io to get input

--- pytactoe/human.py
+++ pytactoe/human.py
@@ -2,5 +2,9 @@ from pytactoe.player import Player
 
 class Human(Player):
 
-    def __init__(self, mark):
+    def __init__(self, mark, io):
         super(Human, self).__init__(mark)
+        self.io = io
+
+    def get_move(self):
+        return self.io.get_input()
--- pytactoe/test/test_human.py
+++ pytactoe/test/test_human.py
@@ -1,9 +1,18 @@
 import unittest
+from mock import MagicMock
 from pytactoe.human import Human
 
 class HumanTests(unittest.TestCase):
 
   def test_human_initializes_with_a_mark(self):
-      human = Human("X")
+      human = Human("X", None)
 
       self.assertEqual(human.mark, "X")
+
+  def test_retrieving_a_move_from_human_player(self):
+      mockio = MagicMock()
+      mockio.get_input = MagicMock(return_value=1)
+      human = Human("X", mockio)
+      chosen_move = human.get_move()
+
+      self.assertEqual(1, chosen_move)
