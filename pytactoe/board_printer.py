68343535bd18546b3d0f841c12aa1639db55566e
Andrew Kelly <kellyaj@gmail.com>
2013-08-20 22:35:55 -0500

    refactors BoardPrinter to be static

--- pytactoe/board_printer.py
+++ pytactoe/board_printer.py
@@ -1,12 +1,10 @@
 class BoardPrinter(object):
 
-    def __init__(self, io):
-        self.io = io
-
-    def print_board(self, board):
+    @staticmethod
+    def print_board(io, board):
         stringified_board = map(str, board)
-        self.io.present("     " + ("   |   ").join(stringified_board[0:3]))
-        self.io.present("    -------------------")
-        self.io.present("     " + ("   |   ").join(stringified_board[3:6]))
-        self.io.present("    -------------------")
-        self.io.present("     " + ("   |   ").join(stringified_board[6:9]))
+        io.present("     " + ("   |   ").join(stringified_board[0:3]))
+        io.present("    -------------------")
+        io.present("     " + ("   |   ").join(stringified_board[3:6]))
+        io.present("    -------------------")
+        io.present("     " + ("   |   ").join(stringified_board[6:9]))
--- pytactoe/test/test_board_printer.py
+++ pytactoe/test/test_board_printer.py
@@ -8,11 +8,10 @@ class BoardPrinterTests(unittest.TestCase):
     def setUp(self):
         self.chosen_out = StringIO()
         self.io = InputOutput(self.chosen_out, StringIO)
-        self.printer = BoardPrinter(self.io)
 
     def test_printing_fresh_board(self):
         board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        self.printer.print_board(board)
+        BoardPrinter.print_board(self.io, board)
         printed_board = ("     1   |   2   |   3\n"
                          "    -------------------\n"
                          "     4   |   5   |   6\n"
@@ -24,7 +23,7 @@ class BoardPrinterTests(unittest.TestCase):
 
     def test_printing_marked_board(self):
         board = ["X", 2, 3, 4, "O", 6, 7, 8, 9]
-        self.printer.print_board(board)
+        BoardPrinter.print_board(self.io, board)
         printed_board = ("     X   |   2   |   3\n"
                          "    -------------------\n"
                          "     4   |   O   |   6\n"
