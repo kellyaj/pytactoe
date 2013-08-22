9363705923898149fbadea0900b96098d6ed1513
Andrew Kelly <kellyaj@gmail.com>
2013-08-22 10:45:17 -0500

    refactors tests for game_loop

--- pytactoe/test/test_game_loop.py
+++ pytactoe/test/test_game_loop.py
@@ -6,20 +6,20 @@ from cStringIO import StringIO
 
 class GameLoopTests(unittest.TestCase):
 
+    def setUp(self):
+        self.player1 = MagicMock()
+        self.player2 = MagicMock()
+
     def test_game_is_created_with_correct_players(self):
-        player1 = MagicMock()
-        player2 = MagicMock()
         io = InputOutput(StringIO(), StringIO())
         game_loop = GameLoop(io)
-        game_loop.create_game(player1, player2)
+        game_loop.create_game(self.player1, self.player2)
 
-        self.assertEqual(player1, game_loop.game.player1)
-        self.assertEqual(player2, game_loop.game.player2)
+        self.assertEqual(self.player1, game_loop.game.player1)
+        self.assertEqual(self.player2, game_loop.game.player2)
 
     def test_evaluting_play_again_prompt(self):
-        player1 = MagicMock()
-        player2 = MagicMock()
         io = InputOutput(StringIO(), StringIO("c\nc\nyes\n"))
         game_loop = GameLoop(io)
-        game_loop.create_game(player1, player2)
+        game_loop.create_game(self.player1, self.player2)
         self.assertTrue(game_loop.play_again())
