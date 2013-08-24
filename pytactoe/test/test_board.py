6403cb7aac6476eba0c24a5b5bcbb5ddf0f92ac7
Andrew Kelly <kellyaj@gmail.com>
2013-08-24 16:04:29 -0500

    refactors and organizes tests

--- pytactoe/test/test_board.py
+++ pytactoe/test/test_board.py
@@ -3,18 +3,40 @@ from pytactoe.board import Board
 
 class BoardTests(unittest.TestCase):
 
+    def setUp(self):
+        self.board = Board()
+        self.default_spots = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    def test_board_initializes_with_spots(self):
+        self.assertEqual(self.default_spots, self.board.spots)
+
     def test_board_can_have_spots_injected(self):
         spots_to_inject = ["X", 2, "O", 4, 5, 6, 7, 8, 9]
         board = Board(spots_to_inject)
 
         self.assertEqual(spots_to_inject, board.spots)
 
-    def setUp(self):
-        self.default_spots = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        self.board = Board()
+    def test_available_spots_list(self):
+        self.assertEqual(self.default_spots, self.board.available_spots())
 
-    def test_board_initializes_with_spots(self):
-        self.assertEqual(self.default_spots, self.board.spots)
+        self.board.place_move("X", 5)
+        available_spots = [1, 2, 3, 4, 6, 7, 8, 9]
+
+        self.assertEqual(available_spots, self.board.available_spots())
+
+    def test_getting_all_rows_columns_and_diagonals(self):
+        expected_rows = [
+                          [1, 2, 3], [4, 5, 6], [7, 8, 9],
+                          [1, 4, 7], [2, 5, 8], [3, 6, 9],
+                          [1, 5, 9], [3, 5, 7]
+                        ]
+
+        self.assertEqual(expected_rows, self.board.get_all_rows())
+
+class BoardMovementTests(unittest.TestCase):
+
+    def setUp(self):
+        self.board = Board()
 
     def test_placing_moves_on_board(self):
         self.board.place_move("X",1)
@@ -26,15 +48,3 @@ class BoardTests(unittest.TestCase):
 
         self.assertTrue(self.board.is_spot_available(1))
         self.assertFalse(self.board.is_spot_available(5))
-
-    def test_available_spots_list(self):
-        self.assertEqual(self.default_spots, self.board.available_spots())
-        self.board.place_move("X", 5)
-
-        self.assertEqual([1, 2, 3, 4, 6, 7, 8, 9], self.board.available_spots())
-
-    def test_getting_all_rows_columns_and_diagonals(self):
-        all_rows = self.board.get_all_rows()
-        expected_rows = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
-
-        self.assertEqual(expected_rows, all_rows)
